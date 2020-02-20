5bf9bbd4e87d69c4b73f862622921fa7597f004c
minhyuk.yang <minhyuk.yang@gmail.com>
2020-02-21 08:16:45 +0900

    modifiied files.

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # django-odqa
 Django for Open-Domain QA
 
+# 절차
+1. 개발 환경 세팅 (Django 설치, pycharm 연동) [x]
+    - [1. 개발환경 세팅하기](https://woolbro.tistory.com/49?category=818996)
+
+2. 하단 장고 예제로 감잡기 [x]
+    - [2. 장고 뷰, url 연동하기](https://woolbro.tistory.com/50?category=818996)
+    - [3. CRUD 추가](https://woolbro.tistory.com/51?category=818996)
+    - [4. 템플릿 확장과 꾸미기](https://woolbro.tistory.com/51?category=818996)
+
+3. bootstrap 이용하여 테마 적용하기 [ ]
+
 # Reference
 - 장고(Django) 예제 / 북마크 프로젝트 [[Github](https://github.com/paullee714/BlogCode/tree/master/Django)]
     - [1. 개발환경 세팅하기](https://woolbro.tistory.com/49?category=818996)
     - [2. 장고 뷰, url 연동하기](https://woolbro.tistory.com/50?category=818996)
     - [3. CRUD 추가](https://woolbro.tistory.com/51?category=818996)
     - [4. 템플릿 확장과 꾸미기](https://woolbro.tistory.com/51?category=818996)
-    
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import pymysql
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookmark',
+    'mrc',
 ]
 
 MIDDLEWARE = [
@@ -76,10 +78,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MRC_TRAIN',
+        'USER': 'mobichat',
+        'PASSWORD': 'chatbot2018!',
+        'HOST': '10.240.252.21',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+        'OPTIONS': {
+                    #'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+                    'sql_mode': 'traditional',
+        }
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
@@ -106,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -119,5 +136,61 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# logging
+LOGGING = {
+    'version': 1,
+    # 기존의 로깅 설정을 비활성화 할 것인가?
+    'disable_existing_loggers': False,
+
+    # 포맷터
+    # 로그 레코드는 최종적으로 텍스트로 표현됨
+    # 이 텍스트의 포맷 형식 정의
+    # 여러 포맷 정의 가능
+    'formatters': {
+        'format1': {
+            'format': '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s',
+            'datefmt': '%d/%b/%Y %H:%M:%S'
+        },
+        'format2': {
+            'format': '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s'
+        },
+        'myFormatt': {
+            'format': '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s'
+        },
+    },
+
+    # 핸들러
+    # 로그 레코드로 무슨 작업을 할 것인지 정의
+    # 여러 핸들러 정의 가능
+    'handlers': {
+        # 로그 파일을 만들어 텍스트로 로그레코드 저장
+        'file': {
+            'level': 'INFO',
+            # 'class': 'logging.FileHandler',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/mrc_create.log'),
+            'when': "midnight", # 매 자정마다
+            'backupCount':5,
+            'formatter': 'myFormatt',
+        },
+        # 콘솔(터미널)에 출력
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'myFormatt',
+        }
+    },
+
+    # 로거
+    # 로그 레코드 저장소
+    # 로거를 이름별로 정의
+    'loggers': {
+        'make_mrc_data': {
+            'handlers': ['file', 'console'],
+            'level': 'DEBUG',
+        }
+    },
+
+}
